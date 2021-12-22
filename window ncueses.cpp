f4e78502cd09d29a3dc66825f6791d5bb52d7d31
Rivaldo2117051054 <rivaldorr547@gmail.com>
2021-12-22 09:02:11 +0700

    membuat window/box dengan ncurses

---
+++ window ncueses.cpp
@@ -0,0 +1,18 @@
+#include <ncurses/ncurses.h>
+using namespace std;
+
+int main(){
+	initscr();
+	
+	WINDOW * win = newwin(20, 30, 1, 1);
+	//window           // menentukan ukuran kotak
+	refresh();
+	//refresh
+	box(win, 0, 0);
+	//ukuran garis tepi/bentuk yg ingin dipakai cth '#'
+	wrefresh(win);
+	//refresh window
+	
+	getch();
+	endwin();
+}
